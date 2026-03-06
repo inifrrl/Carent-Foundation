f95a17198fec8fd8b9c67059baff6c1aba8475ec
inifrrl <164341860+inifrrl@users.noreply.github.com>
2026-03-06 18:28:48 +0700

    Update ProgramMobile.tsx

--- src/imports/ProgramMobile.tsx
+++ src/imports/ProgramMobile.tsx
@@ -305,9 +305,8 @@ export default function ProgramMobile() {
         </svg>
       </div>
       <p className="absolute font-['Work_Sans',sans-serif] font-bold h-[13px] leading-[0] left-[151px] text-[#1e08b1] text-[15.17px] top-[547px] w-[179px] whitespace-pre-wrap">
-        <span className="leading-[12.895px]">{`PROGRAM `}</span>
-        <span className="font-['Work_Sans',sans-serif] font-medium leading-[12.895px]">FOCUS AREA</span>
-      </p>
+        <span className="leading-[12.895px]">{`PROGRAM FOCUS AREA `}</span>
+     
       <p className="absolute font-['Work_Sans',sans-serif] font-medium h-[47px] leading-[0] left-[143px] text-[#1e08b1] text-[6.643px] text-justify top-[567px] w-[214px] whitespace-pre-wrap">
         <span className="leading-[normal]">{`Carent Foundation Indonesia structures its programs around `}</span>
         <span className="font-['Work_Sans',sans-serif] font-bold leading-[normal]">four strategic focus areas</span>
