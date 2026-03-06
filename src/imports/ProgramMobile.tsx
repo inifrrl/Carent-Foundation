eadf96e18bbe2b5429e1049035c26654e295367c
inifrrl <164341860+inifrrl@users.noreply.github.com>
2026-03-06 19:23:46 +0700

    Update ProgramMobile.tsx

--- src/imports/ProgramMobile.tsx
+++ src/imports/ProgramMobile.tsx
@@ -284,7 +284,7 @@ export default function ProgramMobile() {
           <span className="leading-[normal]">that threatens its Indonesia Emas 2045 vision. Despite government efforts, youth unemployment remains high at 16.16%, while nearly 23.79% of youth (aged 16–30) are classified as NEET (Not in Employment, Education, or Training), signaling a failure to transition from education to the labor market.</span>
         </p>
       </div>
-      <div className="-translate-y-full absolute flex flex-col font-['Work_Sans',sans-serif] font-normal h-[87px] justify-end leading-[0] left-[89px] text-[#1e08b1] text-[0px] text-justify top-[310px] tracking-[0.85px] w-[277px]">
+      <div className="-translate-y-full absolute flex flex-col font-['Work_Sans',sans-serif] font-normal h-[55px] justify-end leading-[0] left-[89px] text-[#1e08b1] text-[0px] text-justify top-[310px] tracking-[0.85px] w-[277px]">
         <p className="text-[7px] whitespace-pre-wrap">
           <span className="leading-[normal]">{`The core issue is a severe `}</span>
           <span className="font-['Work_Sans',sans-serif] font-bold leading-[normal]">skills mismatch</span>
